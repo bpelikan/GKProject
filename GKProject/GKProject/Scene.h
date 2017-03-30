da1f1cda6aae0362687edacf21aa637f9dcf18ff
bpelikan <bartoszpelikan@gmail.com>
2017-03-30 19:11:28 +0200

    Add transformation
    Dodano transformacje obiektów po osiach X, Y, Z

--- GKProject/GKProject/Scene.h
+++ GKProject/GKProject/Scene.h
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <windows.h>            // Window defines
+#include <gl\gl.h>              // OpenGL
+#include <math.h>
+#include <gl\glu.h> 
+
 #include "Szescian.h"
 #include "Coordinate.h"
 #include "Stozek.h"
 #include "Walec.h"
+#include "Transformation.h"
 
 class Scene
 {
